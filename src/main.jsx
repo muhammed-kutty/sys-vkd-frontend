88315b0d9e472deb2723981c9f36ea57b9156fcb
muhammed-kutty <124482895+muhammed-kutty@users.noreply.github.com>
2024-11-09 10:36:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-toastify/dist/ReactToastify.css";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';  // Add this line to import the CSS
+//import 'mdb-react-ui-kit/dist/css/mdb.min.css';  // Add this line to import the CSS
 // import 'cdbreact/dist/cdbreact.min.css';
 
 
